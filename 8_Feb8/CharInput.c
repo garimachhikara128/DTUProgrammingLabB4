645ae60329482ebdda6e41ee289119aef70fe71c
Garima <garimachhikara128@gmail.com>
2021-02-10 03:24:11 +0530

    Pattern, Recursion

---
+++ 8_Feb8/CharInput.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int i ;
+    char ch , ec;
+
+    scanf("%d" , &i) ;
+    printf("%d" , i) ;
+
+    //scanf("%d" , &ec) ;
+
+    fflush(stdin) ;
+
+    scanf("%c" , &ch) ;
+    printf("%c" , ch) ;
+
+    return 0  ;
+}
---
+++ 8_Feb8/RecPrintDec.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+void PD(int) ;
+
+int main()
+{
+
+    printf("hello\n") ;
+    PD(3) ;
+    printf("bye\n") ;
+
+    return  0 ;
+}
+
+// bigger problem
+void PD(int n)
+{
+    // base case
+    if(n == 0)
+        return ;
+
+    // work
+    printf("hi %d\n", n) ;
+
+    // smaller problem
+    PD(n-1) ;
+
+    printf("bye %d\n", n) ;
+}
